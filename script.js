c9db9c13212af7c61e31dee71c5ef1798a804a1f
Rio <rogerio.taxlien@gmail.com>
2020-08-17 23:07:45 -0600

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+fetch(url)
+  .then(response => response.json())  
+.then(data => {
+  appendData(data);
+})
+.catch((err) => console.log(err));
+
+function appendData(data) {
+  var mainContainer = document.getElementById("table");
+  for (var i = 0; i < data.length; i++) {
+    var tr = document.createElement("tr");
+    p.innerHTML = '<td>' + data[i].id + '</td><td>' + data[i].userId + '</td><td>' + data[i].title + '</td><td>' + data[i].body + '</td>';
+    mainContainer.appendChild(tr);
+  }
+}
