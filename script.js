4a0387eb82ced01de47290c1c3411319e00e76dc
Rio <rogerio.taxlien@gmail.com>
2020-08-17 23:43:57 -0600

    update

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function appendData(data) {
 }
 
 function filterTable() {
-  console.log(`------INSIDE filterTable!`);
   var input, table, tr, td, tdTxtValue;
   input = document.getElementById("searchBar").value.toUpperCase();
   table = document.getElementById("table");
